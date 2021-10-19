4a4569e887e3deace8d38296870ed05e3e677436
parrya <psawant0015@gmail.com>
2021-10-19 22:49:12 +0530

    file added

---
+++ src/components/pages/Home.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  const [users, setUser] = useState([]);
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  const loadUsers = async () => {
+    const result = await axios.get("http://localhost:3003/users");
+    setUser(result.data.reverse());
+  };
+
+  const deleteUser = async (Id) => {
+    // await axios.delete(`http://localhost:3003/users/${id}`);
+    console.log(Id);
+    console.log(users);
+   const newUserList = users.splice(Id-1, 1);
+   console.log(newUserList);
+    // loadUsers();
+  };
+
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>Home Page</h1>
+        <table class="table border shadow">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">User Name</th>
+              <th scope="col">Email</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr>
+                <th scope="row">{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>
+                {/* View button */}
+                  <Link class="btn btn-primary mr-2" to={`/users/${user.id}`}>
+                    View
+                  </Link>
+                  {/* Edit Button */}
+                  <Link
+                    class="btn btn-outline-primary mr-2"
+                    to={`/users/edit/${user.id}`}
+                  >
+                    Edit
+                  </Link>
+                  {/* Delete button */}
+                  <Link
+                    class="btn btn-danger"
+                    onClick={() => deleteUser(user.id)}
+                  >
+                    Delete
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// not found page used when incorrect data filled in localhost
+const NotFound = () => {
+  return (
+    <div className="not-found">
+      <h1 className="display-1">Page Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
